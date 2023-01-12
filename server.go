2d4df0c481ad88b17e4c350f85fa95f4bdb3be9d
GenFPS <sergeyfeddotov97@gmail.com>
2023-01-12 13:42:51 +0300

    изменил стуктуру рабочей директории

--- server.go
+++ server.go
@@ -15,17 +15,13 @@ func main() {
       
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-
         data := ProductOrder{
             Title: "Производственный заказ",
             Message: "Получить производственный план",
         }
-        tmpl1 := template.Must(template.New("data").Parse(`<div>
-            <h1>{{ .Title}}</h1>
-            <p>{{ .Message}}</p>
-        </div>`))
-
-        tmpl1.Execute(w, data)
+        
+        tmpl, _ :=  template.ParseFiles("templates/index.html")
+        tmpl.Execute(w, data)
     })
  
     fmt.Println("Server is listening...")
